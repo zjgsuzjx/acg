f13bf3bce6134f7be740059b534d28bf7ddb9a8b
zjgsuzjx <zjgsuzjx@163.com>
2023-06-21 10:22:44 +0800

    六月第五次重要更新：追加新网站，修复错误

--- wp-content/themes/webstackpro/js/app.js
+++ wp-content/themes/webstackpro/js/app.js
@@ -178,8 +178,8 @@
                 $(".switch-dark-mode").attr("title","夜间模式");
             $(".mode-ico").removeClass("icon-light");
             $(".mode-ico").addClass("icon-night");
-            $("#bg_canvas").css("width", "0%");
-            $("#bg_canvas").css("height", "0%");
+            $("#bg_canvas").css("width", "100%");
+            $("#bg_canvas").css("height", "100%");
         }
     }
     //返回顶部
